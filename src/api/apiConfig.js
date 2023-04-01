0be9b439567e0eb2e07f23b7d7d5055e8ac270c1
Manoj Kumar <prajapatmanoj609@gmail.com>
2023-04-02 00:59:45 +0530

    first commit

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: 'get from themoviedb.org',
+    apiKey: '4bb73409f3970ca9923f1264a3af4e23',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
